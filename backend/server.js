c81db9248812a7aa6790ea9428dab78a3bf72d09
Geyanth Padmakar <geyanth.2018@gmail.com>
2022-07-27 11:49:49 +0530

    heroku setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 // Instances from other files
 const transactionRoutes = require('./routes/transactions');
@@ -15,14 +16,18 @@ const MONGODB_URI = process.env.MONGO_URI;
 
 // Middleware
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './frontend', './build')));
 
 // Transaction Routes Middleware
 app.use('/api/transactions', transactionRoutes);
 
 // Connecting to MONGODB
 mongoose
-  .connect(MONGODB_URI)
+  .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, './frontend', './build', 'index.html'));
+    });
     // Listen for requests
     app.listen(PORT, () => {
       console.log(`Server is connected DB and Running at the Port ${PORT}`);
